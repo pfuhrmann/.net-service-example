54fd30eed684b491bd3496911a225f13706962e8
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2015-02-13 17:48:17 +0000

    Progress towards working web service

--- Level2Service/SittersService.asmx.cs
+++ Level2Service/SittersService.asmx.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DataModel;
 using System.Linq;
-using System.Web;
 using System.Web.Services;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Level2Service
 {
@@ -14,13 +14,48 @@ namespace Level2Service
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
-    public class SittersService : System.Web.Services.WebService
+    public class SittersService : WebService
     {
 
-        [WebMethod]
-        public string HelloWorld()
+        private readonly DataModelContainer _context;
+
+        public SittersService()
         {
-            return "Hello World";
+            _context = new DataModelContainer();
+        }
+
+        [WebMethod]
+        public XmlDocument Sitters()
+        {
+            var query = _context.Sitters.AsEnumerable();
+
+            // Map DB Sitters to XML
+            XDocument doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("Sitters",
+                  // LINQ query to build the child element.
+                  from c in query
+                  select new XElement("Sitter",
+                    new XAttribute("firstname", c.FirstName),
+                    new XAttribute("lastname", c.LastName)
+                  )
+                )
+            );
+
+            return ToXmlDocument(doc);
+        }
+
+        // Convert XDocument to XMLDocument
+        // http://www.aspnetify.com/2014/04/convert-between-xdocument-and.html
+        public static XmlDocument ToXmlDocument(XDocument xdoc)
+        {
+            var xmlDocument = new XmlDocument();
+            using (var reader = xdoc.CreateReader())
+            {
+                xmlDocument.Load(reader);
+            }
+
+            return xmlDocument;
         }
     }
 }
