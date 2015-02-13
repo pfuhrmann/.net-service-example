848065b33f61e84dc170ca5258ef56e4a87f9e7d
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2015-02-13 18:39:02 +0000

    Sitter detail service working now

--- Level2Service/SittersService.asmx.cs
+++ Level2Service/SittersService.asmx.cs
@@ -7,16 +7,13 @@ using System.Xml.Linq;
 namespace Level2Service
 {
     /// <summary>
-    /// Summary description for SittersService
+    /// Service providing Sitters data parsed from DB
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
-    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
     public class SittersService : WebService
     {
-
         private readonly DataModelContainer _context;
 
         public SittersService()
@@ -29,16 +26,56 @@ namespace Level2Service
         {
             var query = _context.Sitters.AsEnumerable();
 
-            // Map DB Sitters to XML
             XDocument doc = new XDocument(
                 new XDeclaration("1.0", "utf-8", "yes"),
-                new XElement("Sitters",
-                  // LINQ query to build the child element.
-                  from c in query
-                  select new XElement("Sitter",
-                    new XAttribute("firstname", c.FirstName),
-                    new XAttribute("lastname", c.LastName)
-                  )
+                new XElement("sitters",
+                  from sitter in query select 
+                    new XElement("sitter",
+                        new XElement("name",
+                            new XElement("firstname", sitter.FirstName),
+                            new XElement("lastname", sitter.LastName)
+                        ),
+                        new XElement("sitter_id", sitter.Id),
+                        new XElement("email", sitter.Email),
+                        new XElement("phone", sitter.Phone)
+                    )
+                )
+            );
+
+            return ToXmlDocument(doc);
+        }
+
+        [WebMethod]
+        public XmlDocument SitterDetails(int id)
+        {
+            var sitter = _context.Sitters.Find(id);
+
+            XDocument doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("sitter_detail",
+                    new XElement("name",
+                        new XElement("firstname", sitter.FirstName),
+                        new XElement("lastname", sitter.LastName)
+                    ),
+                    new XElement("sitter_id", sitter.Id),
+                    new XElement("email", sitter.Email),
+                    new XElement("phone", sitter.Phone),
+                    new XElement("services",
+                        from service in sitter.Services select
+                          new XElement("service",
+                              new XElement("type", service.Type),
+                              new XElement("location", service.Location),
+                              new XElement("availability", service.Availability),
+                              new XElement("description", service.Description),
+                              new XElement("charges", service.Charges),
+                              new XElement("images",
+                                  from image in service.Images select
+                                    new XElement("image",
+                                        new XElement("image_url", image.code)
+                                    )
+                              )
+                          )
+                     )
                 )
             );
 
